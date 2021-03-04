c76a2fe3c91688d9a1eea934a42a92c338b13059
Just1B <hello@justinbaroux.com>
2021-03-04 11:58:52 +0100

    Add Data type mapping for BQ schemas

--- helpers/types.py
+++ helpers/types.py
@@ -1,12 +1,22 @@
+# https://developers.google.com/google-ads/api/reference/rpc/v6/GoogleAdsFieldDataTypeEnum.GoogleAdsFieldDataType?hl=en
+
+
 def type_to_bq_type(doc_type):
 
     switcher = {
+        "UNSPECIFIED": "STRING",
+        "UNKNOWN": "STRING",
+        "DATE": "DATE",
         "STRING": "STRING",
         "RESOURCE_NAME": "STRING",
         "MESSAGE": "STRING",
         "ENUM": "STRING",
         "DOUBLE": "FLOAT",
+        "FLOAT": "FLOAT",
+        "INT32": "INTEGER",
         "INT64": "INTEGER",
+        "UINT64": "INTEGER",
+        "BOOLEAN": "BOOLEAN",
     }
 
     return switcher.get(doc_type, "")
